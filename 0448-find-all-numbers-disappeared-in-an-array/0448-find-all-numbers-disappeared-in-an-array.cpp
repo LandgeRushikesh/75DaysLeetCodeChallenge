542032a5d031a34d2d8993574fcf18cb381592c0
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-03-15 16:56:49 +0530

    Time: 7 ms (45.33%), Space: 53 MB (90.42%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,20 +1,78 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+
+        // Brute Force Aproach
+        // int n = nums.size();
+        // vector<int> ans;
+
+        // for(int i=1;i<=n;i++){
+        //     bool missing = true;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j]==i){
+        //             missing = false;
+        //         }
+        //     }
+        //     if(missing){
+        //         ans.push_back(i);
+        //     }
+        // }
+
+        // return ans;
+
+        /*
+        Time Complexity - O(n^2)
+
+        Space Complexity - O(n)
+        */
+
+        // Better Approach
+
+        // int n = nums.size();
+        // vector<int> Freq(n+1,0);
+        // vector<int> missing;
+
+        // for(int i=0;i<n;i++){
+        //     Freq[nums[i]]++;
+        // }
+
+        // for(int j=1;j<Freq.size();j++){
+        //     if(Freq[j]==0){
+        //         missing.push_back(j);
+        //     }
+        // }
+
+        // return missing;
+
+        /*
+        Time Complexity - O(n)
+
+        Space Complexity - O(n)
+        */
+
+        // Optimal Approach -
         int n = nums.size();
-        vector<int> Freq(n+1,0);
-        vector<int> missing;
+        vector<int> ans;
 
         for(int i=0;i<n;i++){
-            Freq[nums[i]]++;
-        }
-
-        for(int j=1;j<Freq.size();j++){
-            if(Freq[j]==0){
-                missing.push_back(j);
+            // Mark the index as -ve as there element is present
+            int index=abs(nums[i])-1;
+            if(nums[index]>0){
+                nums[index] = -nums[index];
             }
         }
 
-        return missing;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans.push_back(i+1);
+            }
+        }
+
+        return ans;
+        /*
+        Time Complexity - O(n)
+
+        Space Complexity - O(1)
+        */
     }
 };
