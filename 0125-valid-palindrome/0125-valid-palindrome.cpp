32989f08dd35d124268cafe6ec251f31616ed86f
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-03-19 13:39:43 +0530

    Time: 0 ms (100%), Space: 10 MB (53.7%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    // We can write our own customized alphaNumeric checking function
+    // bool isAplhaNumeric(char ch){
+    //     if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch)<='z')){
+    //         return true;
+    //     }
+    //     return false;  
+    // }
+
+    bool isPalindrome(string s) {
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j){
+            while(!isalnum(s[i]) && i<j){
+                i++;
+            }
+            while(!isalnum(s[j]) && i<j){
+                j--;
+            }
+
+            if(tolower(s[i])!=tolower(s[j])){
+                return false;
+            }
+            i++,j--;
+        }
+
+        return true;
+    }
+};
