a8175f9aee1818561d76bdf7651dbfa37ee3687d
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-03-28 13:22:52 +0530

    Time: 4 ms (55.28%), Space: 10.9 MB (69.74%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26,0);
+        int n = s.length();
+        int left = 0;
+        int right = 0; 
+        int maxFreq = 0;
+        int maxLen = 0;
+
+        while(right<n){
+            freq[s[right]-'A']++;
+            int windowSize = right - left + 1;
+            maxFreq = max(maxFreq,freq[s[right]-'A']);
+
+            if(windowSize-maxFreq > k){
+                freq[s[left]-'A']--;
+                left++;
+            }
+            maxLen = max(maxLen,right - left + 1);
+            right++;
+        }
+        return maxLen;
+    }
+};
