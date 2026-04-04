b7408b3d985a4d9a499c3e6931f0f7203fb2cf21
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-04-04 18:03:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
+    // Brute Force Approach
+    // int searchInsert(vector<int>& nums, int target) {
+    //     int n = nums.size();
+    //     for(int i=0;i<n;i++){
+    //         if(nums[i]==target){
+    //             return i;
+    //         }
+    //         else if(nums[i]>target){
+    //             return i;
+    //         }
+    //     }
+    //     return n;
+    // }
+    
+    // Optimal Approach
     int searchInsert(vector<int>& nums, int target) {
-        // Brute Force Approach
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                return i;
+        int left = 0;
+        int right = n-1;
+
+        while(left<=right){
+            int mid = left + (right-left)/2;
+
+            if(nums[mid]==target){
+                return mid;
             }
-            else if(nums[i]>target){
-                return i;
+            else if(nums[mid]<target){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
             }
         }
-        return n;
+        return left;
     }
 };
