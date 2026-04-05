6c836478711f16e8aee476cf14c47e92de5cdc28
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-04-05 16:08:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,13 +1,43 @@
 class Solution {
 public:
+    bool SearchInRow(vector<vector<int>>& matrix,int row,int col, int target){
+        int sRow = 0;
+        int eRow = col-1;
+
+        while(sRow<=eRow){
+            int mid = (sRow + eRow)/2;
+
+            if(matrix[row][mid]==target){
+                return true;
+            }
+            else if(matrix[row][mid]<target){
+                sRow = mid+1;
+            }
+            else{
+                eRow = mid-1;
+            }
+        }
+        return false;
+    }
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix[0].size();
-        int n = matrix.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int startOfRow = 0;
+        int EndOfRow = col-1;
+
+        while(startOfRow<=EndOfRow){
+            int midOfRow = (startOfRow + EndOfRow)/2;
+
+            if(matrix[midOfRow][0]<=target && matrix[midOfRow][col-1]>=target){
+                return SearchInRow(matrix,midOfRow,col,target);
+            }
+            else if(matrix[midOfRow][0]<target){
+                startOfRow = midOfRow+1;
+            }
+            else{
+                EndOfRow = midOfRow-1;
             }
         }
         return false;
