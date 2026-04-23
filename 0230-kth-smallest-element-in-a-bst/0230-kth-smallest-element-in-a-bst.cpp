03443de4776bac033cec3d61a59fc6136152c4e2
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-04-23 14:13:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,22 +11,48 @@
  */
 class Solution {
 public:
-    void getAllElementsBST(TreeNode* root,vector<int> &ele){
+    // Brute Force Approach -
+
+    // void getAllElementsBST(TreeNode* root,vector<int> &ele){
+    //     if(root==nullptr){
+    //         return;
+    //     }
+
+    //     ele.push_back(root->val);
+    //     getAllElementsBST(root->left,ele);
+    //     getAllElementsBST(root->right,ele);
+    // }
+    // int kthSmallest(TreeNode* root, int k) {
+    //     vector<int> ele;
+    //     getAllElementsBST(root,ele);
+
+    //     // Sort
+    //     sort(ele.begin(),ele.end());
+
+    //     return ele[k-1];
+    // }
+
+    /*
+    Time Complexity - O(nlogn)
+
+    Space Complexity - O(h)
+    */
+
+    // Optimal Approach - inorder traversal returns elements in sorted order
+     void OrderTraversal(TreeNode* root,vector<int>& ele){
         if(root==nullptr){
             return;
         }
 
+        OrderTraversal(root->left,ele);
         ele.push_back(root->val);
-        getAllElementsBST(root->left,ele);
-        getAllElementsBST(root->right,ele);
+        OrderTraversal(root->right,ele);
+        
     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> ele;
-        getAllElementsBST(root,ele);
+       vector<int> ele;
+       OrderTraversal(root,ele);
 
-        // Sort
-        sort(ele.begin(),ele.end());
-
-        return ele[k-1];
+       return ele[k-1];
     }
 };
