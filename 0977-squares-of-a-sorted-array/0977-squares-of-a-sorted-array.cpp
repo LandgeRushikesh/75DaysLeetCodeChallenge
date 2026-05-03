42a3f246eb9429c16f02abba6cdf221aa6909f65
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-05-03 13:43:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     // Brute Force Approach - 
-    vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            nums[i] = nums[i]*nums[i];
-        }
+    // vector<int> sortedSquares(vector<int>& nums) {
+    //     int n = nums.size();
+    //     for(int i=0;i<n;i++){
+    //         nums[i] = nums[i]*nums[i];
+    //     }
 
-        sort(nums.begin(),nums.end());
-        return nums;
-    }
+    //     sort(nums.begin(),nums.end());
+    //     return nums;
+    // }
 
     /*
     Time Complexity - O(nlogn)
@@ -20,4 +20,27 @@ public:
 
     // Optimize approach - as we know largest square will come from either:
     // left(negative largest)
+    // right (positive largest)
+
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n,-1);
+        int left = 0;
+        int right = n-1;
+        int pos = n-1;
+
+        while(left<=right){
+            if(abs(nums[left])>abs(nums[right])){
+                ans[pos] = nums[left]*nums[left];
+                left++;
+            }
+            else{
+                ans[pos] = nums[right]*nums[right];
+                right--;
+            }
+            pos--;
+        }
+
+        return ans;
+    }
 };
