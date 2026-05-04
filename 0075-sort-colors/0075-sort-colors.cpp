8a3a4995d0c8c73b22598fd81dabd4a80360830a
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-05-04 21:53:43 +0530

    Time: 0 ms (100%), Space: 11.7 MB (47.13%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        for(int i=0;i<n;i++){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+            }
+        }
+    }
+};
