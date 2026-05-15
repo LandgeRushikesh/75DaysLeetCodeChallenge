a3158b3fd6b977ef1b5a5e3833f9be09f3a130cc
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-05-15 13:10:30 +0530

    Time: 0 ms (100%), Space: 11 MB (41.05%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,24 +11,49 @@
  */
 class Solution {
 public:
-    void preOrderT(TreeNode* root,vector<int> &preorder){
-       if(root==nullptr){
-        return;
-       }
+    // void preOrderT(TreeNode* root,vector<int> &preorder){
+    //    if(root==nullptr){
+    //     return;
+    //    }
 
-       preorder.push_back(root->val);
-       preOrderT(root->left,preorder);
-       preOrderT(root->right,preorder);
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> preorder;
-        preOrderT(root,preorder);
-        return preorder;
+    //    preorder.push_back(root->val);
+    //    preOrderT(root->left,preorder);
+    //    preOrderT(root->right,preorder);
+    // }
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> preorder;
+    //     preOrderT(root,preorder);
+    //     return preorder;
         
-    }
+    // }
     /*
     Time Complexity - O(n)--->each node visited only once
 
     Space Complexity - O(h)--->recursion stack h=height of the tree
     */
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root==nullptr){
+            return {};
+        }
+        vector<int> preorder;
+        stack<TreeNode*> st;
+
+        st.push(root);
+
+        while(!st.empty()){
+            TreeNode* curr = st.top();
+            st.pop();
+
+            preorder.push_back(curr->val);
+            if(curr->right){
+                st.push(curr->right);
+            }
+            if(curr->left){
+                st.push(curr->left);
+            }
+        }
+        return preorder;
+        
+    }
 };
