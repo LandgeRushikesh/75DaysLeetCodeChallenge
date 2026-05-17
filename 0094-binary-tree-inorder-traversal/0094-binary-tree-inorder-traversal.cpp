bfebf99006b8fec6bd257f9e6484e3f7ade12277
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-05-17 22:38:46 +0530

    Time: 0 ms (100%), Space: 11 MB (33.13%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,18 +11,54 @@
  */
 class Solution {
 public:
-    void inorderT(TreeNode* root,vector<int>& inorder){
-       if(root==nullptr){
-        return;
-       }
+    // void inorderT(TreeNode* root,vector<int>& inorder){
+    //    if(root==nullptr){
+    //     return;
+    //    }
 
-       inorderT(root->left,inorder);
-       inorder.push_back(root->val);
-       inorderT(root->right,inorder);
-    }
+    //    inorderT(root->left,inorder);
+    //    inorder.push_back(root->val);
+    //    inorderT(root->right,inorder);
+    // }
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     vector<int> inorder;
+    //     inorderT(root,inorder);
+    //     return inorder;
+    // }
+    /*
+    Time Complexity - O(n)
+
+    Space Complexity - O(h)
+    */
+
     vector<int> inorderTraversal(TreeNode* root) {
+        if(root==nullptr){
+            return {};
+        }
         vector<int> inorder;
-        inorderT(root,inorder);
+        stack<TreeNode*> st;
+
+        TreeNode* curr = root;
+
+        while(curr != nullptr || !st.empty()){
+
+            // Go to leftmost node
+            while(curr != nullptr){
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            // Process node
+            curr = st.top();
+            st.pop();
+
+            inorder.push_back(curr->val);
+
+            // Move to right subtree
+            curr = curr->right;
+        }
+
+
         return inorder;
     }
 };
