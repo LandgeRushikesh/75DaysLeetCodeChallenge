6e53e5037e1f34eb5e988db2fb82b370482f4725
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-08-25 23:13:46 +0530

    Time: 2 ms (91.95%), Space: 47.7 MB (76.45%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // First find 1st Zero
+        int n = nums.length;
+        int i = -1;
+        for(int j=0;j<n;j++){
+            if(nums[j]==0){
+                i = j;
+                break;
+            }
+        }
+
+        if(i==-1){
+            return;
+        }
+
+        for(int j=i+1;j<n;j++){
+            if(nums[j]!=0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+}
