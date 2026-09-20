7bf0be2ea0c7250ac42aa8aec3b8cffdb39c6125
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-09-20 14:37:58 +0530

    Time: 0 ms (100%), Space: 20.8 MB (81.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+// Brute Force Approach -
+
+/*
     void setRowColZero(vector<vector<int>>& matrix,int row,int col){//in params the row and col is just a number of row and column which we have to modify
         int row_size = matrix.size();
         int col_size = matrix[0].size();
@@ -15,6 +18,8 @@ public:
         }
 
     }
+
+
     void setZeroes(vector<vector<int>>& matrix) {
         int row = matrix.size();
         int col = matrix[0].size();
@@ -34,6 +39,33 @@ public:
             for(int j=0;j<col;j++){
                 if(check[i][j] == true){
                     setRowColZero(matrix,i,j);
+                }
+            }
+        }
+    }
+
+*/
+void setZeroes(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<bool> rows(row,false);
+        vector<bool> cols(col,false);
+
+        // Here if matrix[i][j] is 0 then set particular row and col in specified vector as zero
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        // now traverse matrix if particular row or col has zero then set the particular matrix[i][j] = 0
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(rows[i] || cols[j]){
+                    matrix[i][j] = 0;
                 }
             }
         }
