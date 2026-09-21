d8886a2e403f921442b128dc670fd16dd6b84123
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-09-21 14:59:03 +0530

    Time: 0 ms (100%), Space: 9.1 MB (30.3%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
+    // vector<int> getRow(int rowIndex) {
+    //     vector<vector<int>> triangle;
+    //     for(int i=0;i<=rowIndex;i++){
+    //         triangle.push_back({});// this is to create a row in vector as we cannot access vector using indices as it's size will be zero so it will give reference to NULL error.
+    //         for(int j=0;j<=i;j++){
+    //             if(j==0 || j==i){
+
+    //                 triangle[i].push_back(1);
+    //                 continue;
+    //             }
+
+    //             int ele = triangle[i-1][j] + triangle[i-1][j-1];
+
+    //             triangle[i].push_back(ele);
+    //         }
+    //     }
+
+    //     return triangle[rowIndex];
+    // }
     vector<int> getRow(int rowIndex) {
-        vector<vector<int>> triangle;
-        for(int i=0;i<=rowIndex;i++){
-            triangle.push_back({});// this is to create a row in vector as we cannot access vector using indices as it's size will be zero so it will give reference to NULL error.
+        vector<int> prev_row = {1};
+        vector<int> curr_row;
+        for(int i=1;i<=rowIndex;i++){
+            curr_row.clear();
+            curr_row.resize(i+1);
             for(int j=0;j<=i;j++){
                 if(j==0 || j==i){
-
-                    triangle[i].push_back(1);
+                    curr_row[j] = 1;
                     continue;
                 }
 
-                int ele = triangle[i-1][j] + triangle[i-1][j-1];
-
-                triangle[i].push_back(ele);
+                int ele = prev_row[j] + prev_row[j-1];
+                curr_row[j] = ele;
             }
+            prev_row = curr_row;
         }
-
-        return triangle[rowIndex];
+        if(curr_row.size()==0){
+            return {1};
+        }
+        return curr_row;
     }
 };
