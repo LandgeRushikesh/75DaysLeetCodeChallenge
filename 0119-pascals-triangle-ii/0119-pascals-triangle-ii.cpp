156882c2a1dd8a42f6d54f6a6509950a213bfdeb
Rushikesh Landge <152911547+LandgeRushikesh@users.noreply.github.com>
2026-09-21 14:41:57 +0530

    Time: 0 ms (100%), Space: 9.4 MB (5.58%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> triangle;
+        for(int i=0;i<=rowIndex;i++){
+            triangle.push_back({});// this is to create a row in vector as we cannot access vector using indices as it's size will be zero so it will give reference to NULL error.
+            for(int j=0;j<=i;j++){
+                if(j==0){
+                }
+                if(j==0 || j==i){
+
+                    triangle[i].push_back(1);
+                    continue;
+                }
+
+                int ele = triangle[i-1][j] + triangle[i-1][j-1];
+
+                triangle[i].push_back(ele);
+            }
+        }
+
+        return triangle[rowIndex];
+    }
+};
